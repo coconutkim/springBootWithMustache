d6a9e253558f86c430e7e6e9ea59f52bacb0a714
coconutkim <achaeyeon18@gmail.com>
2024-11-28 19:37:03 +0900

    1128

--- src/main/java/com/example/springBoot_selfStudy/controller/ArticleController.java
+++ src/main/java/com/example/springBoot_selfStudy/controller/ArticleController.java
@@ -3,11 +3,16 @@ package com.example.springBoot_selfStudy.controller;
 import com.example.springBoot_selfStudy.dto.ArticleForm;
 import com.example.springBoot_selfStudy.entity.Article;
 import com.example.springBoot_selfStudy.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j //로깅 기능으로 로그를 찍으면 나중에라도 그동안 찍힌 로그를 찾아볼 수 있다
+//simple logging facade for java
 @Controller //컨트롤러임을 선언한다
 public class ArticleController {
 
@@ -24,14 +29,31 @@ public class ArticleController {
     public String createArticle(ArticleForm form){
         //폼 데이터를 dto로 받기
 
-        System.out.println(form.toString()); //dto에 폼 데이터가 담겼는지 출력값을 통해 확인
+        log.info(form.toString());
+//        System.out.println(form.toString()); //dto에 폼 데이터가 담겼는지 출력값을 통해 확인
 
         Article article = form.toEntity();
-        System.out.println(article.toString()); //dto가 엔티티로 잘 변환되는지 확인
+        log.info(article.toString());
+        //System.out.println(article.toString()); //dto가 엔티티로 잘 변환되는지 확인
 
         Article saved = articleRepository.save(article);
-        //crudrepository를 상송받았기에 가능한 기능
-        System.out.println(saved.toString()); //article이 데이터베이스에 저장되는지 확인
+        //crudrepository를 상속받았기에 가능한 기능
+        log.info(saved.toString());
+        //System.out.println(saved.toString()); //article이 데이터베이스에 저장되는지 확인
         return "";
+    }
+
+    @GetMapping("/articles/{id}") //데이터 조회 요청을 접수한다
+    //컨트롤러에서 url 변수를 사용할 때는 중괄호 하나만 사용한다
+    public String show(@PathVariable Long id, Model model){ //매개변수로 id를 받는다
+        //위의 어노테이션은 url 요청으로 들어온 전달값을 컨트롤러의 매개변수로 가져온다
+        log.info("id = " + id);
+
+        //아이디를 조회해 데이터를 가져온다
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        //해당 아이디 값이 없으면 null을 반환하라
+        //모델에 데이터를 등록한다
+        model.addAttribute("article", articleEntity);
+        return "articles/show";
     }
 }
--- src/main/java/com/example/springBoot_selfStudy/entity/Article.java
+++ src/main/java/com/example/springBoot_selfStudy/entity/Article.java
@@ -4,7 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@NoArgsConstructor //기본 생성자 추가 어노테이션
+@ToString
 @Entity
 //jpa가 제공하는 어노테이션
 // 이 클래스를 기반으로 데이터베이스에 테이블이 생성된다
@@ -19,20 +25,4 @@ public class Article {
 
     @Column
     private String content;
-
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
-
 }
